35c4c6d1af4c8da38bc6ab14bacff213a0c1542a
creisle <crreisle@gmail.com>
2014-05-27 11:25:19 -0700

    initial file added

---
+++ pq.c
@@ -0,0 +1,38 @@
+/*
+PQ trees implementation
+ref: booth and lueker, 1976
+ref: sarah young, 1977
+
+a first attempt at implementing pq tree for testing graph planarity
+
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// define some constants
+
+#define TRUE        1
+#define FALSE       0
+
+/*
+ * 1. create p-nodes, q-nodes and c-nodes
+ * 2. create reduction templates
+ * 3. ...
+ *
+ *
+ *
+ *
+ *
+ */
+
+struct node {
+    node *parent;
+    int value;
+};
+
+int main(){
+    printf("start of program\n");
+    return 0;
+}
